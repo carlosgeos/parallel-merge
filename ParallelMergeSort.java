8bfdddba830fdf5ee86cc546ae3e54aaf0882d88
Carlos Requena López <crequena@ulb.ac.be>
2016-08-15 19:40:56 +0200

    basic parallar mergesort

--- ParallelMergeSort.java
+++ ParallelMergeSort.java
@@ -8,42 +8,66 @@ import java.lang.*;
 import java.lang.reflect.Array;import java.util.*;
 import java.util.concurrent.*;
 import java.util.Arrays;
-import java.lang.reflect.ParameterizedType;
+//import java.lang.reflect.ParameterizedType;
 
 public class ParallelMergeSort<T> {
 
-    protected static final int ORDER = 1000;
-    protected static final int INIT_SIZE = 60000;
-
-    static Class<Integer> esetipo ;
+    static Class<?> component_type;
 
     public static void main(String args[]) {
-	int size = INIT_SIZE;
 	System.out.println("\n\nParallel sorting:");
 	System.out.println("-----------------");
 
-	List<Integer> arr = new ArrayList<>(Arrays.asList(5,2,4,3,6,1));
-	esetipo = Integer.class;
+	List<Integer> arr = new ArrayList<>(Arrays.asList(5,2,4,6,1,7,3,8));
+	//List<Integer> arr = new ArrayList<>(Arrays.asList(5,2,4,8,1,7,3,6));
+	component_type = Integer.class;
 
-	seq_merge(arr, arr.size());
+	Thread main_thread = new Thread(new Runnable() {
+		public void run() {
+		    ParallelMergeSort.merge(arr, arr.size());
+		}
+	    });
+	main_thread.start();
 	display(arr);
     }
 
-    public static <T extends Comparable<? super T>> void seq_merge(List<T> v, int size) {
+    public static <T extends Comparable<? super T>> void merge(List<T> v, int size) {
+	System.out.println("------ THREAD : " + "num" + "------------------");
+	System.out.print("I've got: ");
+	display(v);
+	System.out.println("");
 	if (v.size() > 1) {
 	    final int half_point = size / 2;
 	    List<T> left = v.subList(0, half_point);
 	    List<T> right = v.subList(half_point, size);
 
-	    seq_merge(left, half_point);
-            seq_merge(right, size - half_point);
+	    // Create two new threads for each side
+	    Thread left_thread = new Thread(new Runnable() {
+		    public void run() {
+			ParallelMergeSort.merge(left, half_point);
+		    }
+		});
+	    left_thread.start();
+	    System.out.println("THREAD: " + left_thread.getName() + " STARTED");
 
-	    System.out.println("------------------------");
-	    System.out.print("I've got: ");
-	    display(v);
-	    System.out.println("");
+	    Thread right_thread = new Thread(new Runnable() {
+		    public void run() {
+			ParallelMergeSort.merge(right, size - half_point);
+		    }
+		});
+	    right_thread.start();
 
-	    T[] working_list = (T[]) Array.newInstance(esetipo, half_point);
+	    // Wait for them to finish
+	    try {
+		right_thread.join();
+		left_thread.join();
+	    } catch (InterruptedException e) {
+		e.printStackTrace();
+	    }
+
+
+	    // This is the auxiliary space needed for mergesort : O(n).
+	    T[] working_list = (T[]) Array.newInstance(component_type, half_point);
 
 	    int i = 0, j = 0, k = 0;
 
@@ -53,11 +77,6 @@ public class ParallelMergeSort<T> {
 
             while (j < half_point && i < size) {
 		v.set(k++, v.get(i).compareTo(working_list[j]) < 0 ? v.get(i++) : working_list[j++]);
-		// if (v.get(i).compareTo(working_list[j]) < 0) {
-		//     v.set(k++, v.get(i++));
-		// } else {
-		//     v.set(k++, working_list[j++]);
-		// }
 		display(v);
             }
 
